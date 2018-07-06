fe615b29261b45dcbe950c14b410f78ab1ebd7d8
freeLTC <maikel@plubr.com.br>
2018-07-06 08:05:16 -0700

    ui

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,33 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
+/*var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+};*/
+
+var erc721 = artifacts.require("./ERC721.sol");
+var ownable = artifacts.require("./Ownable.sol");
+var pirateattack = artifacts.require("./PirateAttack.sol");
+var piratefactory = artifacts.require("./PirateFactory.sol");
+var piratefeeding = artifacts.require("./PirateFeeding.sol");
+var piratehelper = artifacts.require("./PirateHelper.sol");
+var pirateownership = artifacts.require("./PirateOwnership.sol");
+var safemath = artifacts.require("./SafeMath.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(safemath);
+  //deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(ownable);
+  deployer.deploy(erc721);
+  
+  deployer.deploy(piratefactory);
+  deployer.deploy(piratefeeding);
+  deployer.deploy(piratehelper);
+  deployer.deploy(pirateattack);
+  deployer.deploy(pirateownership);
+  
 };
+
+
